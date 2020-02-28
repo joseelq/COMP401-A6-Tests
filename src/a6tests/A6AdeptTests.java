1baa4a48f1b5d6cd9d0a01c8c80d98d45378e323
kmayerpatel <kmp@cs.unc.edu>
2020-02-28 14:40:46 -0500

    Initial commit

---
+++ src/a6tests/A6AdeptTests.java
@@ -0,0 +1,87 @@
+package a6tests;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import a6.*;
+
+
+// MPAP = MutablePixelArrayPicture
+
+public class A6AdeptTests {
+	
+	@Test
+	public void testMPAPArrayConstructor() {
+		// Tests the array form of the constructor.
+		fail("Replace with testing code");
+	}
+	
+	@Test
+	public void testMPAPWidthHeightInitValueConstructor() {
+		// Tests the form of the constructor that takes in width, height, and initial pixel
+		fail("Replace with testing code");
+	}
+
+	@Test
+	public void testMPAPWidthHeightConstructor() {
+		// Tests the form of the constructor that takes in width and height
+		fail("Replace with testing code");
+	}
+
+	@Test
+	public void testMPAPDimensionGetters() {
+		// Tests getWidth() adn getHeight()
+		fail("Replace with testing code");
+	}
+
+	@Test
+	public void testMPAPGetPixel() {
+		// Creates a picture from a 2D array of pixels with different pixels in various places.
+		// Tests that pixel returned from getPixel at those places returns the same pixels
+		fail("Replace with testing code");
+	}
+	
+	@Test
+	public void testMPAPPaintSinglePixel() {
+		// Tests paint(x,y,p) and paint(x,y,p,f)
+		fail("Replace with testing code");
+	}
+	
+	
+	@Test
+	public void testMPAPPaintRectangle() {
+		// Tests paint(ax, ay, bx, by, p) and paint(ax, ay, bx, by, p, f)
+		fail("Replace with testing code");
+	}
+	
+	@Test
+	public void testMPAPPaintCircle() {
+		// Tests paint(cx, cy, radius, p) and paint(cx, cy, radius, p, f);
+		fail("Replace with testing code");
+	}
+	
+	@Test
+	public void testMonochromePictureConstructor() {
+		// Tests constructor
+		fail("Replace with testing code");
+	}
+	
+	@Test
+	public void testMonochromePictureGetPixel() {
+		// Tests that the pixel returned by getPixel is the same one given to the constructor
+		fail("Replace with testing code");
+	}
+	
+	@Test
+	public void testMonochromePicturePaintFail() {
+		// Tests that trying to paint on a monochrome picture throws an exception of the
+		// appropriate type
+		fail("Replace with testing code");
+	}
+	
+	@Test
+	public void testMonochromePictureDimensionGetters() {
+		// Tests getWidth() adn getHeight()
+		fail("Replace with testing code");
+	}
+
+}
---
+++ src/a6tests/A6JediTests.java
@@ -0,0 +1,111 @@
+package a6tests;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import a6.*;
+
+public class A6JediTests {
+
+	@Test
+	public void testGradientPictureConstructor() {
+		// Tests the constructor for GradientPicture
+		fail("Replace with testing code");
+	}
+
+	@Test
+	public void testGradientPictureGetPixel() {
+		// Tests that the pixel returned by getPixel is the same one given to the constructor
+		fail("Replace with testing code");
+	}
+	
+	@Test
+	public void testGradientPaintFail() {
+		// Tests that trying to paint on a monochrome picture throws an exception of the
+		// appropriate type
+		fail("Replace with testing code");
+	}
+	
+	@Test
+	public void testGradientPictureDimensionGetters() {
+		// Tests getWidth() and getHeight()
+		fail("Replace with testing code");
+	}
+
+	@Test
+	public void testHorizontalStackPictureConstructor() {
+		// Tests horizontal stack picture constructor
+		fail("Replace with testing code");
+	}	
+	
+	@Test
+	public void testHorizontalStackPictureDimensionGetters() {
+		// Tests getWidth() and getHeight()
+		fail("Replace with testing code");
+	}
+
+	@Test
+	public void testHorizontalStackPictureGetPixel() {
+		// Creates a picture from a 2D array of pixels with different pixels in various places.
+		// Tests that pixel returned from getPixel at those places returns the same pixels
+		fail("Replace with testing code");
+	}
+	
+	@Test
+	public void testHorizontalStackPicturePaintSinglePixel() {
+		// Tests paint(x,y,p) and paint(x,y,p,f)
+		fail("Replace with testing code");
+	}
+	
+	
+	@Test
+	public void testHorizontalStackPicturePaintRectangle() {
+		// Tests paint(ax, ay, bx, by, p) and paint(ax, ay, bx, by, p, f)
+		fail("Replace with testing code");
+	}
+	
+	@Test
+	public void testHorizontalStackPicturePaintCircle() {
+		// Tests paint(cx, cy, radius, p) and paint(cx, cy, radius, p, f);
+		fail("Replace with testing code");
+	}
+
+	@Test
+	public void testVerticalStackPictureConstructor() {
+		// Tests vertical stack picture constructor
+		fail("Replace with testing code");
+	}	
+	
+	@Test
+	public void testVerticalStackPictureDimensionGetters() {
+		// Tests getWidth() and getHeight()
+		fail("Replace with testing code");
+	}
+
+	@Test
+	public void testVerticalStackPictureGetPixel() {
+		// Creates a picture from a 2D array of pixels with different pixels in various places.
+		// Tests that pixel returned from getPixel at those places returns the same pixels
+		fail("Replace with testing code");
+	}
+	
+	@Test
+	public void testVerticalStackPicturePaintSinglePixel() {
+		// Tests paint(x,y,p) and paint(x,y,p,f)
+		fail("Replace with testing code");
+	}
+	
+	
+	@Test
+	public void testVerticalStackPicturePaintRectangle() {
+		// Tests paint(ax, ay, bx, by, p) and paint(ax, ay, bx, by, p, f)
+		fail("Replace with testing code");
+	}
+	
+	@Test
+	public void testVerticalStackPicturePaintCircle() {
+		// Tests paint(cx, cy, radius, p) and paint(cx, cy, radius, p, f);
+		fail("Replace with testing code");
+	}
+
+
+}
